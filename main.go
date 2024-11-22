7a3edaa8a4aaf6ed6f07cdb9b50e86ebeee2cc9e
mrkiz-git <mark.elman@gmail.com>
2024-11-22 13:19:28 +0200

    command login created

--- main.go
+++ main.go
@@ -4,21 +4,39 @@ import (
 	"fmt"
 	"log"
 	"mrkiz-git/gator/internal/config"
+	"os"
 )
 
-func main() {
+type state struct {
+	cfg *config.Config
+}
 
+func main() {
 	cfg, err := config.LoadConfig()
 	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
-	cfg.SetUser("mark")
-	cfg, err = config.LoadConfig()
+	// cretae a pointer to state, every time programState w be passed, it will be passed to *state
+	programState := &state{
+		cfg: cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
+		log.Fatal(err)
 	}
-
-	fmt.Printf("%+v\n", *cfg)
-
 }
